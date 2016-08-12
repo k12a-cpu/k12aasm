ea076bd1d842fdf0bad7737efde75f1879b2e238
Kier Davis <kierdavis@gmail.com>
2016-08-12 23:35:45 +0100

    Neaten up the size() function in assignaddresses.nim

--- k12a/assembler/passes/assignaddresses.nim
+++ k12a/assembler/passes/assignaddresses.nim
@@ -2,14 +2,10 @@ import ../types
 
 proc size(item: Item): uint16 {.noSideEffect.} =
   case item.kind
-  of itemInstruction:
-    2
-  of itemLabel:
-    0
-  of itemByte:
-    1
-  of itemWord:
-    2
+  of itemInstruction: 2
+  of itemLabel: 0
+  of itemByte: 1
+  of itemWord: 2
 
 proc assignAddresses*(unit: CompilationUnit) {.noSideEffect.} =
   var address: uint16 = 0
